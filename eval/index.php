1b40286594335dc889d8f4739559f35fad4375a2
geronimow <75610038+geronimow@users.noreply.github.com>
2021-01-22 04:48:20 +0100

    Add files via upload

---
+++ eval/index.php
@@ -0,0 +1,8 @@
+<h1> Bienvenue sur l'index ! </h1>
+<img src = "https://static.yabiladi.com/files/accounts/albums/30df1b75a176e6ceafdf24ff9000c867_thumb_565.jpg">
+
+<?php
+
+session_start();
+
+?>
---
+++ eval/template/register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>eval_projet_php</title>
+    <link href = "stylesheet" href ="style/style.css">
+  </head>
+  <body>
+    <footer>
+      <div>
+        <br>
+        <form method = "POST">
+          <input type ="email" name ="email"> <br>
+          <input type ="password" name ="password"> <br>
+          <input type ="submit" value ="Inscription">
+          </form>
+      </div>
+    </footer>
+  </body>
+</html>
